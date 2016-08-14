e1e5619ec2bec553ce1005f67fa036860594b117
gongjiyun <keep_myself@outlook.com>
2016-08-14 16:29:45 +0800

    delete

---
+++ webapp/src/test/java/mytest/nio/PipesTest.java
@@ -0,0 +1,9 @@
+package mytest.nio;
+
+public class PipesTest {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- webapp/src/test/java/mytest/socket/Server.java
+++ webapp/src/test/java/mytest/socket/Server.java
@@ -3,10 +3,65 @@ This class add by Administrator
 */
 package mytest.socket;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	public static void main(String[] args) {
-		Socket sk = new Socket();
+	public static void main(String[] args) throws Exception{
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket();
+			serverSocket.bind(new InetSocketAddress("localhost", 8040));
+			while(true){
+				Socket socket = null;
+				try {
+					socket = serverSocket.accept();
+					if(socket!=null){
+						InputStream is = socket.getInputStream();
+						BufferedInputStream bfin = new BufferedInputStream(is);
+						InputStreamReader ins = new InputStreamReader(is);
+						BufferedReader reader = new BufferedReader(ins);
+						
+						OutputStream os = socket.getOutputStream();
+						OutputStreamWriter osw = new OutputStreamWriter(os);
+						BufferedWriter writer = new BufferedWriter(osw);
+						
+						FileInputStream fin = new FileInputStream("/Projects/TestData/small.txt");
+						InputStreamReader freader = new InputStreamReader(fin);
+						BufferedReader bfreader = new BufferedReader(freader);
+						
+						String message = null;
+						while((message = bfreader.readLine())!=null){
+							writer.write(message);
+							writer.flush();
+						}
+						bfreader.close();
+						freader.close();
+						fin.close();
+						
+						writer.close();
+					}
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				socket.close();
+				System.out.println("\n");
+				System.out.println("finish read & write");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			serverSocket.close();
+		}
 	}
 }

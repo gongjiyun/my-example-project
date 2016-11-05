10d97159565cbe84e9834e780447e5d435d8dbe4
gongjiyun <keep_myself@outlook.com>
2016-11-05 23:27:59 +0800

    test

---
+++ cloud-frameworks/src/main/java/org/apache/spark/examples/streaming/StreamingExamples.java
@@ -0,0 +1,10 @@
+package org.apache.spark.examples.streaming;
+
+/**
+ * Created by Administrator on 2016-10-25.
+ */
+public class StreamingExamples {
+    public static void setStreamingLogLevels() {
+
+    }
+}

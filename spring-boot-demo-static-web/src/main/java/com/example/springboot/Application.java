5dab0b131b11a3eea1ce1ee4363db089ad1fa88d
gongjiyun <keep_myself@outlook.com>
2017-12-20 22:11:22 +0800

    test

--- spring-boot-demo-static-web/src/main/java/com/example/springboot/Application.java
+++ spring-boot-demo-static-web/src/main/java/com/example/springboot/Application.java
@@ -28,7 +28,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootConfiguration
-@EnableJpaRepositories(basePackages={"com.example.springboot.repositories.jpa"})
+@EnableJpaRepositories(basePackages = { "com.example.springboot.repositories.jpa" })
 public class Application extends org.springframework.boot.web.servlet.support.SpringBootServletInitializer {
 
 	public static void main(String[] args) throws Exception {
@@ -40,4 +40,5 @@ public class Application extends org.springframework.boot.web.servlet.support.Sp
 		return application.sources(Application.class);
 	}
 
+	
 }

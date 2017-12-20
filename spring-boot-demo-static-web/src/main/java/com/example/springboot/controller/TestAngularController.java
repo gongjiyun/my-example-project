7bd22a1c42e27d877566802f6b0ff3c46c31e88f
gongjiyun <keep_myself@outlook.com>
2017-12-20 22:41:54 +0800

    test

--- spring-boot-demo-static-web/src/main/java/com/example/springboot/controller/TestAngularController.java
+++ spring-boot-demo-static-web/src/main/java/com/example/springboot/controller/TestAngularController.java
@@ -1,22 +1,16 @@
 package com.example.springboot.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.service.UserService;
-import com.example.springboot.vo.User;
 
-@Controller
+@RestController
 public class TestAngularController extends BaseController{
 	
 	@Autowired
@@ -35,32 +29,5 @@ public class TestAngularController extends BaseController{
         return "angular-test";
 	}
 	
-	@ResponseBody
-	@RequestMapping(value="/api/listAllUsers", method={RequestMethod.GET, RequestMethod.POST})
-	public List<User> listAllUsers(HttpServletRequest request, HttpServletResponse response) throws Exception{
-        List<com.example.springboot.entity.User> users = userService.findAll();
-        if(users==null){
-        	return null;
-        }
-        List<User> userlist = new ArrayList<User>();
-        users.forEach(user->{
-        	
-        	User u = new User();
-        	u.setUid(user.getUid());
-        	u.setName(user.getUsername());
-        	u.setEmail(user.getEmail());
-        	userlist.add(u);
-        });
-        
-        return userlist;
-	}
-	
-	
-	@ResponseBody
-	@RequestMapping(value="/api/check/username", method={RequestMethod.GET, RequestMethod.POST})
-	public String unique(@RequestBody String param,  HttpServletRequest request, HttpServletResponse response) throws Exception{
-        System.out.println("param : " + param);
-		return "{\"isUnique\":true}";
-	}
 
 }
--- spring-boot-demo-static-web/src/main/java/com/example/springboot/controller/UserController.java
+++ spring-boot-demo-static-web/src/main/java/com/example/springboot/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.springboot.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,13 +17,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.springboot.entity.User;
 import com.example.springboot.service.UserService;
 
-
 @Controller
-public class UserController extends BaseController{
-	
+public class UserController extends BaseController {
+
 	@Autowired
 	private UserService userService;
-	
 
 	public UserService getUserService() {
 		return userService;
@@ -31,23 +31,52 @@ public class UserController extends BaseController{
 		this.userService = userService;
 	}
 
-	@RequestMapping(path="/user/list", method={RequestMethod.GET, RequestMethod.POST})
+	@RequestMapping(path = "/user/list", method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public List<User> listUsers(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception{
+	public List<User> listUsers(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws Exception {
 		return userService.findAll();
 	}
-	
-	@RequestMapping(path="/user/create", method={RequestMethod.GET, RequestMethod.POST})
+
+	@RequestMapping(path = "/user/create", method = { RequestMethod.GET, RequestMethod.POST })
 	@ResponseBody
-	public String createUser(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception{
+	public String createUser(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
 		User user = new User();
 		user.setUid(1l);
 		user.setUsername("test");
 		user.setEmail("test@cn.cm");
-		
+
 		User exist = userService.create(user);
-		
+
 		return exist.getUsername();
 	}
-	
+
+	@RequestMapping(value = "/api/listAllUsers", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public List<User> listAllUsers(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		List<User> users = userService.findAll();
+		if (users == null) {
+			return null;
+		}
+		List<User> userlist = new ArrayList<User>();
+		users.forEach(user -> {
+
+			User u = new User();
+			u.setUid(user.getUid());
+			u.setUsername(user.getUsername());
+			u.setEmail(user.getEmail());
+			userlist.add(u);
+		});
+
+		return userlist;
+	}
+
+	@RequestMapping(value = "/api/check/username", method = { RequestMethod.GET, RequestMethod.POST })
+	@ResponseBody
+	public String unique(@RequestBody String param, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		System.out.println("param : " + param);
+		return "{\"isUnique\":true}";
+	}
+
 }

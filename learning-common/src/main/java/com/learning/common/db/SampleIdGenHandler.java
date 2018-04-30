6d5ded7993100306d9854ce02bb8535d6d1d39d2
gongjiyun <keep_myself@outlook.com>
2018-04-30 09:44:26 +0800

    refator codes

---
+++ learning-common/src/main/java/com/learning/common/db/SampleIdGenHandler.java
@@ -0,0 +1,8 @@
+package com.learning.common.db;
+
+public class SampleIdGenHandler implements IdGenHandler {
+    @Override
+    public String generate() throws Exception {
+        return null;
+    }
+}
